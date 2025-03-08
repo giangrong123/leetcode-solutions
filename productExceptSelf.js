a0720aadd8486434235e36122022ce55d1971030
giangrong123 <156559817+giangrong123@users.noreply.github.com>
2025-03-09 00:10:46 +0700

    Update productExceptSelf.js

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -1,1 +1,25 @@
 // LeetCode 238: Product of Array Except Self
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let n = nums.length;
+    let LProduct = new Array(n).fill(1);
+    let RProduct = new Array(n).fill(1);
+    let answer = new Array(n);
+    
+    for(let i = 1; i < n; i++) {
+        LProduct[i] = LProduct[i - 1] * nums[i - 1];
+    }
+
+    for(let i = n - 2; i >= 0; i--) {
+        RProduct[i] = RProduct[i + 1] * nums[i + 1];
+    }
+
+    for(let i = 0; i < n; i++) {
+        answer[i] = LProduct[i] * RProduct[i];
+    }
+
+    return answer;
+};
