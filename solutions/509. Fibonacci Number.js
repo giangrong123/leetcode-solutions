e4cc4029e3c76ff68ea08ad2cda2605ef5f703dc
giangrong123 <156559817+giangrong123@users.noreply.github.com>
2025-03-23 12:34:37 +0700

    Update 509. Fibonacci Number.js

--- solutions/509. Fibonacci Number.js
+++ solutions/509. Fibonacci Number.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n, memo = {}) {
+var fib = function(n) {
     if (n <= 1) return n;
     let prev = 0, curr = 1;
     for(let i = 2; i <= n; i++) {
