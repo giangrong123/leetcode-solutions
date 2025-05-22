e6851360989d80d2e7d8476c1ddb9a1391a9f11f
giangrong123 <156559817+giangrong123@users.noreply.github.com>
2025-05-22 09:33:08 +0700

    Create 0118. Pascal's Triangle.js

---
+++ solutions/0118. Pascal's Triangle.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+  let res = [];
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        row.push(1);
+      } else {
+      row.push(res[i - 1][j - 1] + res[i - 1][j]);
+      }
+    }
+    res.push(row);
+  }
+  return res;
+};
